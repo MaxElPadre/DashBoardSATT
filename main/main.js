379ec0c15e568fd3b5bd9961baa406b59bed0df2
JCE <jerome.celebi@ynov.com>
2019-12-16 18:52:29 +0100

    fixing colors :art:

--- main/main.js
+++ main/main.js
@@ -1,21 +1,73 @@
 document.addEventListener("DOMContentLoaded", () => {
     let radarChart = document.getElementById("radarChart");
     initChartRadar(radarChart);
+
+    let barCanvar = document.getElementById("barChart");
+    initBarChart(barCanvar);
 });
+
+function initBarChart(barCanvar) {
+    let data = {
+        labels: ["2020", "2021", "2022", "2023", "TOTAL"],
+        datasets: [{
+            label: "Objective",
+            backgroundColor: "#494e89",
+            data: [3.65, 2.57, 2.43, 3.02, 6.54],
+            roundedBar: 50
+        }, {
+            label: "Expected",
+            backgroundColor: "#4f57bb",
+            data: [2.87, 1.81, 1.78, 2.67, 5.70]
+        }, {
+            label: "Obtained",
+            backgroundColor: "#6873ff",
+            data: [1.98, 1.24, 1.55, 2.35, 3.62]
+        }]
+    };
+
+    let myBarChart = new Chart(barCanvar, {
+        type: 'bar',
+        data: data,
+        options: {
+            scaleShowLabels: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        display: false
+                    },
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        min: 0,
+                    },
+                    gridLines: {
+                        display: false
+                    },
+                    categoryPercentage: 0.5,
+                    barPercentage: 0.2
+                }]
+            }
+        }
+    });
+}
 
 function initChartRadar(marksCanvas) {
     let marksData = {
         labels: ["Leadership", "Roadmap", "Infrastructure", "Workforce", "Strategic Value", "Personal drive"],
         datasets: [{
             backgroundColor: "transparent",
-            borderColor: "rgba(200,0,0,0.6)",
+            borderColor: "#e6be7a",
             fill: false,
             radius: 6,
-            pointRadius: 6,
-            pointBorderWidth: 3,
-            pointBackgroundColor: "orange",
-            pointBorderColor: "rgba(200,0,0,0.6)",
-            pointHoverRadius: 10,
+            pointRadius: 4,
+            pointBorderWidth: 1,
+            pointBackgroundColor: ["#c20a0a", "#c20a0a", "#f79d5e", "#c20a0a", "#2fc0a3", "#f79d5e"],
+            pointBorderColor: ["#c20a0a", "#c20a0a", "#f79d5e", "#c20a0a", "#2fc0a3", "#f79d5e"],
+            pointHoverRadius: 8,
             data: [65, 75, 70, 80, 60, 80]
         }]
     };
@@ -29,11 +81,13 @@ function initChartRadar(marksCanvas) {
                 stepSize: 20
             },
             pointLabels: {
-                fontSize: 10
+                fontSize: 10,
+                fontColor: ["#c20a0a", "#c20a0a", "#f79d5e", "#c20a0a", "#2fc0a3", "#f79d5e"]
             }
         },
         legend: {
-            display: false
+            display: false,
+
         }
     };
 
